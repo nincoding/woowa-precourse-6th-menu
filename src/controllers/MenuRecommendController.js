59097208a3341ae5c480334853dd3868e14b6937
한수지 <fatholic5@gmail.com>
2023-11-16 12:24:06 +0900

    :sparkles: feat: 못먹는 메뉴의 입력 검증 실행 및 재입력 로직 추가 이름별로 못먹는 메뉴를 입력받아 검증한 뒤 객체에 저장시킴

--- src/controllers/MenuRecommendController.js
+++ src/controllers/MenuRecommendController.js
@@ -1,4 +1,5 @@
 import NameValidator from '../domain/validators/NameValidator.js';
+import HateMenuValidator from '../domain/validators/HateMenuValidator.js';
 import InputView from '../views/InputView.js';
 import OutputView from '../views/OutputView.js';
 import errorHandler from '../utils/errorHandler.js';
@@ -6,6 +7,7 @@ import errorHandler from '../utils/errorHandler.js';
 class MenuRecommendController {
   #service;
   #names;
+  #coachsHateMenus = {};
 
   constructor(MenuRecommend) {
     this.#service = MenuRecommend;
@@ -16,13 +18,14 @@ class MenuRecommendController {
   async startService() {
     await this.#initService();
 
-    console.log(this.#names, '유효한이름들');
+    console.log(this.#coachsHateMenus);
   }
 
   async #initService() {
     const printError = (message) => OutputView.printErrorMessage(message);
 
     await errorHandler(async () => await this.#requireValidNames(), printError);
+    await errorHandler(async () => await this.#requireValidHateMenus(), printError);
   }
 
   async #requireValidNames() {
@@ -31,6 +34,16 @@ class MenuRecommendController {
     NameValidator.validate(inputNames);
 
     this.#names = inputNames;
+  }
+
+  async #requireValidHateMenus() {
+    for (const name of this.#names) {
+      const hateMenus = await InputView.getHateMenus(name);
+
+      HateMenuValidator.validate(hateMenus);
+
+      this.#coachsHateMenus[name] = hateMenus;
+    }
   }
 }
 

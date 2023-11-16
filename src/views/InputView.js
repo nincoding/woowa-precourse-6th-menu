73667e0be9e4b4b96368d1622b9a0273dac57faa
한수지 <fatholic5@gmail.com>
2023-11-16 12:20:35 +0900

    :sparkles: feat: 못먹는 메뉴들을 입력받는 기능 추가 문자열을 입력받으면 콤마를 기준으로 배열로 만들어 반환한다.

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -12,6 +12,17 @@ const InputView = {
 
     return parseString(coachNames);
   },
+
+  /**
+   * 이름을 전달받아, 해당 이름을 가진 코치가 못먹는 음식을 입력받는다.
+   * @param {string} name
+   * @returns {String[]} - 못먹는 음식들
+   */
+  async getHateMenus(name) {
+    const hateMenus = await Console.readLineAsync(INPUT_MESSAGE.requireHateMenus(name));
+
+    return parseString(hateMenus);
+  },
 };
 
 export default InputView;

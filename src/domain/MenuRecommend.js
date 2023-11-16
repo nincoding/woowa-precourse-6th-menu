aedb0e033f39e64a36003b13fd8f31011852bc61
한수지 <fatholic5@gmail.com>
2023-11-16 21:18:05 +0900

    :sparkles: feat: 메뉴추천기능 추가

--- src/domain/MenuRecommend.js
+++ src/domain/MenuRecommend.js
@@ -1,5 +1,34 @@
+import MenuMaker from './models/MenuMaker.js';
+
 class MenuRecommend {
-  constructor() {}
+  #coach;
+  #hateMenus = [];
+  #recommendedCategory;
+  #recommendedMenus = [];
+
+  constructor(coachHateMenus, recommendedCategory) {
+    this.#coach = Object.keys(coachHateMenus);
+    this.#hateMenus = coachHateMenus[this.#coach];
+    this.#recommendedCategory = recommendedCategory;
+    this.#createRecommendMenus();
+  }
+
+  getRecommendMenus() {
+    return this.#recommendedMenus;
+  }
+
+  #createRecommendMenus() {
+    for (let i = 0; i < 5; i++) {
+      const menuMaker = new MenuMaker(
+        this.#recommendedCategory[i],
+        this.#hateMenus,
+        this.#recommendedMenus
+      );
+      const recommendMenu = menuMaker.getMenu();
+
+      this.#recommendedMenus.push(recommendMenu);
+    }
+  }
 }
 
 export default MenuRecommend;

5d7599126b2e17b03d76d7fac52a3df2302a0cf8
한수지 <fatholic5@gmail.com>
2023-11-16 17:00:55 +0900

    :sparkles: feat: 랜덤 카테고리 생성기능 추가 랜덤한 값을 생성하여 카테고리와 매칭시킨다.

--- src/domain/models/CategoryMaker.js
+++ src/domain/models/CategoryMaker.js
@@ -1,5 +1,31 @@
+import { Random } from '@woowacourse/mission-utils';
+
 class CategoryMaker {
-  constructor() {}
+  #number;
+  #category;
+
+  constructor() {
+    this.#number = this.#makeRandomNumber();
+    this.#category = this.#calcCategory();
+  }
+
+  getCategory() {
+    return this.#category;
+  }
+
+  #makeRandomNumber() {
+    const number = Random.pickNumberInRange(1, 5);
+
+    return number;
+  }
+
+  #calcCategory() {
+    if (this.#number === 1) return '일식';
+    if (this.#number === 2) return '한식';
+    if (this.#number === 3) return '중식';
+    if (this.#number === 4) return '아시안';
+    if (this.#number === 5) return '양식';
+  }
 }
 
 export default CategoryMaker;

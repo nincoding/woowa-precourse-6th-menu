e0e1e067b3fe7fe521f55b665e7d85ad9512f250
한수지 <fatholic5@gmail.com>
2023-11-16 11:25:52 +0900

    :sparkles: feat: 코치들의 이름을 입력받는 기능 추가 문자열을 입력받으면 콤마를 기준으로 배열로 만들어 값을 반환함

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,5 +1,17 @@
+import { Console } from '@woowacourse/mission-utils';
+import parseString from '../utils/parseString.js';
+import { INPUT_MESSAGE } from '../constants/messages.js';
+
 const InputView = {
-  async getNames() {},
+  /**
+   * 코치들의 이름을 입력받는다.
+   * @returns {String[]} - 코치들의 이름
+   */
+  async getNames() {
+    const coachNames = await Console.readLineAsync(INPUT_MESSAGE.requireNames);
+
+    return parseString(coachNames);
+  },
 };
 
 export default InputView;

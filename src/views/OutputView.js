48d616c63034b97594a03f44fbbf27b870720d89
한수지 <fatholic5@gmail.com>
2023-11-16 11:26:20 +0900

    :sparkles: feat: 시작문구와 에러문구 출력기능 추가

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,5 +1,14 @@
+import { Console } from '@woowacourse/mission-utils';
+import { OUTPUT_MESSAGE } from '../constants/messages.js';
+
 const OutputView = {
-  print() {},
+  printErrorMessage(message) {
+    Console.print(message);
+  },
+
+  printStartMessage() {
+    Console.print(OUTPUT_MESSAGE.start);
+  },
 };
 
 export default OutputView;

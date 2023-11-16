998e2a19fc867414865ab0acebdb8173df92cbd6
한수지 <fatholic5@gmail.com>
2023-11-16 12:23:11 +0900

    :recycle: refactor: 메서드명 오타수정

--- src/domain/validators/NameValidator.js
+++ src/domain/validators/NameValidator.js
@@ -4,10 +4,10 @@ import { VALID_CONDITION } from '../../constants/conditions.js';
 
 class NameValidator {
   static validate(names) {
-    throwValidationError(!this.isValidLength(names), ERROR_MESSAGE.invaildLength);
-    throwValidationError(!this.isVaildMinMember(names), ERROR_MESSAGE.invaildMinMember);
-    throwValidationError(!this.isVaildMaxMember(names), ERROR_MESSAGE.invaildMaxMember);
-    throwValidationError(!this.isVaildUniqueName(names), ERROR_MESSAGE.invalidUnique);
+    throwValidationError(!this.isValidLength(names), ERROR_MESSAGE.invalidLength);
+    throwValidationError(!this.isValidMinMember(names), ERROR_MESSAGE.invalidMinMember);
+    throwValidationError(!this.isValidMaxMember(names), ERROR_MESSAGE.invalidMaxMember);
+    throwValidationError(!this.isValidUniqueName(names), ERROR_MESSAGE.invalidUnique);
   }
 
   // 모든 이름의 길이가 유효한지 검증한다.
@@ -20,21 +20,21 @@ class NameValidator {
   }
 
   // 멤버의 수가 최소 숫자 이상인지 검증한다.
-  static isVaildMinMember(names) {
+  static isValidMinMember(names) {
     const memberCount = names.length;
 
     return memberCount >= VALID_CONDITION.minMemberCount;
   }
 
   // 멤버의 수가 최대 숫자 이하인지 검증한다.
-  static isVaildMaxMember(names) {
+  static isValidMaxMember(names) {
     const memberCount = names.length;
 
     return memberCount <= VALID_CONDITION.maxMemberCount;
   }
 
   // 이름이 중복되지 않은지 검증한다.
-  static isVaildUniqueName(names) {
+  static isValidUniqueName(names) {
     const uniqueNames = new Set(names);
 
     return uniqueNames.size === names.length;

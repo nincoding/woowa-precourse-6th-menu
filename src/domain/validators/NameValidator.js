db262298d1a0f92c355e1992ce8c59d372a390d9
한수지 <fatholic5@gmail.com>
2023-11-16 11:24:53 +0900

    :sparkles: feat: 이름의 유효성 검증 기능 추가 이름들이 모두 길이에 맞는지 검증한다, 멤버수의 최소값과 최대값이 조건에 맞는지 검증한다, 이름들에 중복이 있는지 검증한다 유효하지 않을 경우 해당 에러메시지와 함께 에러를 내보낸다

--- src/domain/validators/NameValidator.js
+++ src/domain/validators/NameValidator.js
@@ -1,5 +1,44 @@
+import throwValidationError from '../../utils/throwValidationError.js';
+import { ERROR_MESSAGE } from '../../constants/messages.js';
+import { VALID_CONDITION } from '../../constants/conditions.js';
+
 class NameValidator {
-  constructor() {}
+  static validate(names) {
+    throwValidationError(!this.isValidLength(names), ERROR_MESSAGE.invaildLength);
+    throwValidationError(!this.isVaildMinMember(names), ERROR_MESSAGE.invaildMinMember);
+    throwValidationError(!this.isVaildMaxMember(names), ERROR_MESSAGE.invaildMaxMember);
+    throwValidationError(!this.isVaildUniqueName(names), ERROR_MESSAGE.invalidUnique);
+  }
+
+  // 모든 이름의 길이가 유효한지 검증한다.
+  static isValidLength(names) {
+    return names.every((name) => {
+      return (
+        name.length >= VALID_CONDITION.minNameLength && name.length <= VALID_CONDITION.maxNameLength
+      );
+    });
+  }
+
+  // 멤버의 수가 최소 숫자 이상인지 검증한다.
+  static isVaildMinMember(names) {
+    const memberCount = names.length;
+
+    return memberCount >= VALID_CONDITION.minMemberCount;
+  }
+
+  // 멤버의 수가 최대 숫자 이하인지 검증한다.
+  static isVaildMaxMember(names) {
+    const memberCount = names.length;
+
+    return memberCount <= VALID_CONDITION.maxMemberCount;
+  }
+
+  // 이름이 중복되지 않은지 검증한다.
+  static isVaildUniqueName(names) {
+    const uniqueNames = new Set(names);
+
+    return uniqueNames.size === names.length;
+  }
 }
 
 export default NameValidator;

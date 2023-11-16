fb40916a89842ae7bad18b244a71dae87cac1fdf
한수지 <fatholic5@gmail.com>
2023-11-16 21:18:35 +0900

    :recycle: refactor: 메세지 줄바꿈 추가

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,7 +1,7 @@
 const OUTPUT_MESSAGE = {
   start: '점심 메뉴 추천을 시작합니다.',
   resultMessage: '\n메뉴 추천 결과입니다.',
-  endMessage: '추천을 완료했습니다.',
+  endMessage: '\n추천을 완료했습니다.\n',
 };
 
 const INPUT_MESSAGE = {

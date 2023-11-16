8911a2266ef0bbf7c20830e593621f4d516d5ad6
한수지 <fatholic5@gmail.com>
2023-11-16 17:02:21 +0900

    :sparkles: feat: 추천메뉴 생성 기능 추가 추천 카테고리에 맞는 메뉴를 랜덤으로 반환한다. 싫어하는메뉴와 이미 추천한 메뉴에 생성한 추천메뉴가 포함된 경우 다시 재생성한다

--- src/domain/models/MenuMaker.js
+++ src/domain/models/MenuMaker.js
@@ -1,5 +1,39 @@
+import { Random } from '@woowacourse/mission-utils';
+import parseString from '../../utils/parseString.js';
+import { MENU } from '../../constants/constant.js';
+
 class MenuMaker {
-  constructor() {}
+  #category;
+  #hateMenus;
+  #recommendedMenus;
+  #menu;
+
+  constructor(category, hateMenus, recommendedMenus) {
+    this.#category = category;
+    this.#hateMenus = hateMenus;
+    this.#recommendedMenus = recommendedMenus;
+    this.#menu = this.#recommendMenu(category);
+  }
+
+  getMenu() {
+    while (this.#isValidCotainMenu()) {
+      this.#menu = this.#recommendMenu(this.#category);
+    }
+
+    return this.#menu;
+  }
+
+  #recommendMenu(category) {
+    const menus = parseString(MENU[category]);
+    const menusIndex = menus.map((_, index) => index);
+    const recommendMenuIndex = Random.shuffle(menusIndex)[0];
+
+    return menus[recommendMenuIndex];
+  }
+
+  #isValidCotainMenu() {
+    return this.#hateMenus.includes(this.#menu) || this.#recommendedMenus.includes(this.#menu);
+  }
 }
 
 export default MenuMaker;

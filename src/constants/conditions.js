d2460a98518c1c86df9ac82f0e3698edc7b43b1c
한수지 <fatholic5@gmail.com>
2023-11-16 11:21:30 +0900

    :sparkles: feat: 메세지 상수화 및 검증조건 상수 추가

---
+++ src/constants/conditions.js
@@ -0,0 +1,8 @@
+const VALID_CONDITION = {
+  minNameLength: 2,
+  maxNameLength: 4,
+  minMemberCount: 2,
+  maxMemberCount: 5,
+};
+
+export { VALID_CONDITION };
--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,3 +1,16 @@
-const OUTPUT_MESSAGE = {};
+const OUTPUT_MESSAGE = {
+  start: '점심 메뉴 추천을 시작합니다.',
+};
 
-export { OUTPUT_MESSAGE };
+const INPUT_MESSAGE = {
+  requireNames: '코치의 이름을 입력해 주세요. (, 로 구분)\n',
+};
+
+const ERROR_MESSAGE = {
+  invaildLength: '[ERROR] 이름은 최소 2글자, 최대 4글자 입력해야 합니다.',
+  invaildMinMember: '[ERROR] 코치는 최소 2명 이상 입력해야 합니다.',
+  invaildMaxMember: '[ERROR] 코치는 최대 5명 이하 입력해야 합니다.',
+  invalidUnique: '[ERROR] 이름은 중복되지 않아야 합니다.',
+};
+
+export { OUTPUT_MESSAGE, INPUT_MESSAGE, ERROR_MESSAGE };

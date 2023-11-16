e08792a7170eade482e53e6e5dfec1781ed6bab8
한수지 <fatholic5@gmail.com>
2023-11-16 12:21:13 +0900

    :sparkles: feat: 상수 추가 및 상수명 수정

--- src/constants/conditions.js
+++ src/constants/conditions.js
@@ -1,8 +1,9 @@
-const VALID_CONDITION = {
+const VALID_CONDITION = Object.freeze({
   minNameLength: 2,
   maxNameLength: 4,
   minMemberCount: 2,
   maxMemberCount: 5,
-};
+  maxhateMenuCount: 2,
+});
 
 export { VALID_CONDITION };
--- src/constants/messages.js
+++ src/constants/messages.js
@@ -4,13 +4,16 @@ const OUTPUT_MESSAGE = {
 
 const INPUT_MESSAGE = {
   requireNames: '코치의 이름을 입력해 주세요. (, 로 구분)\n',
+  requireHateMenus: (name) => `${name}(이)가 못 먹는 메뉴를 입력해 주세요.\n`,
 };
 
 const ERROR_MESSAGE = {
-  invaildLength: '[ERROR] 이름은 최소 2글자, 최대 4글자 입력해야 합니다.',
-  invaildMinMember: '[ERROR] 코치는 최소 2명 이상 입력해야 합니다.',
-  invaildMaxMember: '[ERROR] 코치는 최대 5명 이하 입력해야 합니다.',
-  invalidUnique: '[ERROR] 이름은 중복되지 않아야 합니다.',
+  invalidLength: '[ERROR] 이름은 최소 2글자, 최대 4글자 입력해야 합니다.',
+  invalidMinMember: '[ERROR] 코치는 최소 2명 이상 입력해야 합니다.',
+  invalidMaxMember: '[ERROR] 코치는 최대 5명 이하 입력해야 합니다.',
+  invalidUnique: '[ERROR] 중복되지 않아야 합니다.',
+  invalidMaxCount: '[ERROR] 못먹는 메뉴는 최대 2개 이하여야 합니다.',
+  invalidContainMenu: '[ERROR] 메뉴판에 못먹는 메뉴가 존재하지 않습니다.',
 };
 
 export { OUTPUT_MESSAGE, INPUT_MESSAGE, ERROR_MESSAGE };

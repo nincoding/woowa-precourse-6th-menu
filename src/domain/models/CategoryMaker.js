7dfc6338ac9f59bf193338986438c3703a749782
한수지 <fatholic5@gmail.com>
2023-11-16 17:25:16 +0900

    :sparkles: feat: 카테고리가 최대2회를 넘어갈시 재생성 기능추가

--- src/domain/models/CategoryMaker.js
+++ src/domain/models/CategoryMaker.js
@@ -1,15 +1,22 @@
 import { Random } from '@woowacourse/mission-utils';
 
 class CategoryMaker {
+  #recommendedCategory;
   #number;
   #category;
 
-  constructor() {
+  constructor(recommendedCategory) {
+    this.#recommendedCategory = recommendedCategory;
     this.#number = this.#makeRandomNumber();
     this.#category = this.#calcCategory();
   }
 
   getCategory() {
+    while (this.#isContainCategory()) {
+      this.#number = this.#makeRandomNumber();
+      this.#category = this.#calcCategory();
+    }
+
     return this.#category;
   }
 
@@ -25,6 +32,14 @@ class CategoryMaker {
     if (this.#number === 3) return '중식';
     if (this.#number === 4) return '아시안';
     if (this.#number === 5) return '양식';
+  }
+
+  #isContainCategory() {
+    const count = this.#recommendedCategory.filter(
+      (category) => category === this.#category
+    ).length;
+
+    return count > 1;
   }
 }
 

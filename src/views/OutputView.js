8535e5c18052b3988d366cfb12eb103b95924c04
한수지 <fatholic5@gmail.com>
2023-11-16 21:17:33 +0900

    :sparkles: feat: 추천메뉴 출력템플릿 기능 추가

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -13,6 +13,33 @@ const OutputView = {
   printResultMessage() {
     Console.print(OUTPUT_MESSAGE.resultMessage);
   },
+
+  printGenerateMenuTable(data) {
+    const keys = Object.keys(data);
+    const categoriesIndex = keys.indexOf('카테고리');
+    const coachNames = keys.filter((key) => key !== '구분' && key !== '카테고리');
+
+    const table = [
+      ['구분', '카테고리', ...coachNames],
+      ...data['구분'].map((_, index) => [
+        data['구분'][index],
+        data['카테고리'][index],
+        ...coachNames.map((coachName) => data[coachName][index]),
+      ]),
+    ];
+
+    const transposedTable = this.transpose(table);
+
+    transposedTable.forEach((row) => Console.print(`[ ${row.join(' | ')} ]`));
+  },
+
+  transpose(matrix) {
+    return matrix[0].map((_, colIndex) => matrix.map((row) => row[colIndex]));
+  },
+
+  printEndMessage() {
+    Console.print(OUTPUT_MESSAGE.endMessage);
+  },
 };
 
 export default OutputView;
